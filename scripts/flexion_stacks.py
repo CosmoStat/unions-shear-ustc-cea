7745cadcf2f5acf48aea0747d4575b05544a9083
martinkilbinger <martinkilbinger@cea.fr>
2025-12-01 14:03:14 +0100

    better path handing; added sigma for g, F, G

--- scripts/flexion_stacks.py
+++ scripts/flexion_stacks.py
@@ -18,15 +18,15 @@ from unions_wl import run
 
 
 # %%
-def set_params_in(shape):
+def set_params_in(shape, input_dir=".", sig_shape=-1):
     
     params_in = {}
 
     params_in["shape"] = shape
 
     # Input catalogue names
-    params_in["input_path_fg"] = "fg.fits"
-    params_in["input_path_bg"] = f"bg_{shape}.fits"
+    params_in["input_path_fg"] = f"{input_dir}/fg.fits"
+    params_in["input_path_bg"] = f"{input_dir}/bg_{shape}.fits"
 
     # Other paramters
     params_in["key_ra_fg"] = "RA"
@@ -38,6 +38,8 @@ def set_params_in(shape):
     elif shape == "G":
         params_in["key_e1"] = "t1"
         params_in["key_e2"] = "t2"
+        
+    params_in["sig_shape"] = sig_shape
 
     params_in["key_w_bg"] = "w"
     params_in["verbose"] = True
@@ -46,13 +48,13 @@ def set_params_in(shape):
     return params_in
 
 # %%
-def run_and_plot(obj, params_in, shape, ax=None, mode="angular", stack="auto"):
+def run_and_plot(obj, params_in, ax=None, mode="angular", stack="auto"):
  
-    out_base = f"{shape}_cl_{mode}_{stack}"
-
     # Set instance parameters, copy from above
     for key in params_in:
         obj._params[key] = params_in[key]
+
+    out_base = f"{obj._params['shape']}_cl_{mode}_{stack}"
 
     obj._params["scales"] = mode
     obj._params["stack"] = stack
@@ -62,17 +64,23 @@ def run_and_plot(obj, params_in, shape, ax=None, mode="angular", stack="auto"):
     obj._params["n_theta"] = 20
     obj._params["out_path"] = f"{out_base}.fits"
     
+    if obj._params["stack"] == "post":
+        obj._params["out_path_jk"] = f"{out_base}_jk.fits"
+    
     if mode == "angular":
         obj._params["npatch"] = 15
     else:
         obj._params["npatch"] = 1
     
-    obj.run()
+    if os.path.exists(obj._params["out_path"]):
+        obj.read_correlation()
+    else:
+        obj.run()
 
     if ax is not None:
         # Mosaic plot: Don't save
         out_base = None
-    obj.plot_EB(out_base=out_base, ax=ax, shape=shape)
+    obj.plot_EB(out_base=out_base, ax=ax)
 
 
 # %%
@@ -85,6 +93,14 @@ cases = [
 
 mosaic_plot = True
 
+sig_shape = {
+    "gamma": 0.26,
+    "F": 0.04,
+    "G": 0.04,
+}
+
+input_dir = ".."
+
 for mode, stack in cases:
 
     if mosaic_plot:
@@ -92,7 +108,7 @@ for mode, stack in cases:
         fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(30, 10))
 
     for idx, shape in enumerate(("gamma", "F", "G")):
-        params_in = set_params_in(shape)
+        params_in = set_params_in(shape, input_dir=input_dir, sig_shape=sig_shape[shape])
 
         # Create compute_ng instance
         obj = run.Compute_NG()
@@ -105,10 +121,11 @@ for mode, stack in cases:
             ax = axes[idx]
 
         print("Running case:", shape, mode, stack)
-        run_and_plot(obj, params_in, shape, ax=ax, mode=mode, stack=stack)
+        run_and_plot(obj, params_in, ax=ax, mode=mode, stack=stack)
 
     if mosaic_plot:
         plt.suptitle(f"coordinates={mode}, stack={stack}")
+        plt.tight_layout()
         cs_plots.savefig(f"cl_{mode}_{stack}.png")
 
 # %%
